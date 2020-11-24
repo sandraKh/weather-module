101ccc654f96c7c91a86aef2a7ffcb8160f508f6
Sandra Kullar <41023080+sandraKh@users.noreply.github.com>
2020-11-24 12:36:56 +0100

    Update WeatherTest.php

--- test/Controller/WeatherTest.php
+++ test/Controller/WeatherTest.php
@@ -5,51 +5,116 @@ namespace Anax\Weather;
 use Anax\DI\DIFactoryConfig;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Test the IpAPIControllerTest.
+ */
 class WeatherTest extends TestCase
 {
 
+    /**
+     * Prepare before each test.
+     */
     protected function setUp()
     {
         global $di;
 
-        $di = new DIFactoryConfig();
+        $this->di = new DIFactoryConfig();
+        $di = $this->di;
         $di->loadServices(ANAX_INSTALL_PATH . "/config/di");
         $di->loadServices(ANAX_INSTALL_PATH . "/test/config/di");
 
-        $this->di = $di;
-
         $this->controller = new WeatherController();
-        $this->controller->setDI($this->di);
+        $this->controller->setDI($di);
         $this->controller->initialize();
     }
 
-    public function testgetInfo()
+    public function testLocation()
     {
-        $session = $this->di->get("session");
-
+        $this->di->get("request")->setGet("city", "Paris");
         $res = $this->controller->getInfo();
         $this->assertNotNull($res);
-        $session->destroy();
+    }
+
+    public function testQuery()
+    {
+        $this->di->get("request")->setGet("city", "Paris");
+        $res = $this->controller->getQuery();
+        $this->assertequals($res, null);
+
+
+        $this->di->get("request")->setGet("Paris");
+
+        $res1 = $this->controller->indexAction();
+        $this->assertIsObject($res1);
+
+
+        $weatherModel = $this->di->get("weatherhelper");
+        $data = $weatherModel->getUsersThroughMultiCurl([1], "Paris");
+        $this->assertNotNull($data);
+
     }
 
     public function testgetInfoIpv6()
-    {
-        $session = $this->di->get("session");
-        $session->set("ipaddress", "Paris");
+        {
+            $session = $this->di->get("session");
+            $session->set("ipaddress", "Paris");
 
-        $res = $this->controller->getInfo();
+            $res = $this->controller->getInfo();
 
-        $this->assertNotNull($res);
-        $session->destroy();
-    }
+            $this->assertNotNull($res);
+            $session->destroy();
+        }
 
-    public function testIndexActionGet()
-    {
-        $this->di->get("request")->setGet("ipaddress", "Paris");
-
-        $res = $this->controller->indexAction();
-        $this->assertIsObject($res);
-        $this->assertInstanceOf("Anax\Response\Response", $res);
-        $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
-    }
+    // public function testIndexAction()
+    // {
+    //     $this->di->get("request")->setGet("Paris");
+    //     $res = $this->controller->indexAction();
+    //     $this->assertNotNull($res);
+    // }
 }
+
+
+// class Weather2Test extends TestCase
+// {
+//
+//     protected function setUp()
+//     {
+//         global $di;
+//
+//         $this->di = new DIFactoryConfig();
+//         $di = $this->di;
+//         $di->loadServices(ANAX_INSTALL_PATH . "/config/di");
+//         $di->loadServices(ANAX_INSTALL_PATH . "/test/config/di");
+//
+//         $this->controller = new WeatherController();
+//         $this->controller->setDI($di);
+//         //$this->controller->initialize();
+//     }
+//
+//     // public function testgetInfo()
+//     // {
+//     //     $session = $this->di->get("session");
+//     //
+//     //     $res = $this->controller->getInfo();
+//     //     $this->assertNotNull($res);
+//     //     $session->destroy();
+//     // }
+//
+//     // public function testgetInfoIpv6()
+//     // {
+//     //     $session = $this->di->get("session");
+//     //     $session->set("ipaddress", "Paris");
+//     //
+//     //     $res = $this->controller->getInfo();
+//     //
+//     //     $this->assertNotNull($res);
+//     //     $session->destroy();
+//     // }
+//
+//     public function testIndexAction()
+//     {
+//         $this->di->get("request")->setGet("city", "Paris");
+//         $res = $this->controller->indexAction();
+//         $this->assertNotNull($res);
+//     }
+// }
